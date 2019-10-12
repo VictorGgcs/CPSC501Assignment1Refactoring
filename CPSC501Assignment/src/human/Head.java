d1e19d10720bea4377dd4457c710d45e8c365335
Victor <victor.guan@ucalgary.ca>
2019-10-11 21:31:30 -0600

    Refactoring 3 Replace Temp with Query

--- CPSC501Assignment/src/human/Head.java
+++ CPSC501Assignment/src/human/Head.java
@@ -4,22 +4,31 @@ import java.util.Random;
 public class Head extends BodyPart {
 	int energy;
 	int life;
+	private Random ran;
+	private String[] posIdea;
 	
 	
 	public Head() {
 		this.energy = 100;
 		System.out.print(this.energy);
 		this.life = 100;
+		this.ran = new Random();
+		this.posIdea = new String[] {"Life","Purpose","Food","Work","Pizza","Computers"};
 	}
 	
 	
 	public String think(){
 		this.energy -= 10;
-		String[] posIdea = {("Life"),("Purpose"),("Food"),("Work"),("Pizza"),("Computers")};
-		Random ran = new Random();
-		String thought = posIdea[ran.nextInt(6)];
+		String thought = chooseRandomWord();
 		System.out.println("You thought about " + thought);
 		return thought;
+	}
+	
+	
+	
+	
+	public String chooseRandomWord() {
+		return posIdea[ran.nextInt(6)];
 	}
 	
 	
